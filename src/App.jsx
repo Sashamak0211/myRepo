900ad841712e5d8d70bcdc90e09f14ebdd4f556f
123 123 <mak12121212@gmail.com>
2025-04-10 11:36:16 +0300

    add nowYear logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const newDate = new Date()
+const newData = new Date()
 function App() {
   const [count, setCount] = useState(0)
 
@@ -33,7 +33,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <div className="nowData">
-        <p> {`${newDate.getFullYear()}`} </p>
+        {/* забыл добавить комиты */}
+        <p> {`${newData.getFullYear()}`} </p>
       </div>
     </>
   )
