61cfa685f7821a8e8ac142a448e99fdcef1961ce
123 123 <mak12121212@gmail.com>
2025-04-10 12:37:35 +0300

    rework App from JS

--- src/apps.jsx
+++ src/apps.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -83,52 +83,21 @@ const reactLogo = reactLogo.createElement('a', {
         {className: "nowData"},
         createYearP,
         
-     );
+     )
 
-     
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  //   <>
-  //     <div>
-  //       {/* imperativniy */}
-  //       <a href="https://vite.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       {/* декларативный */}
-  //       <button onClick={() => setCount((count) => count + 1)}> 
-  //         count is {count}
-  //       </button>
-  //       {/* imper */}
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //     <div className="nowData">
-  //       {/* забыл добавить комиты */}
-  //       <p> {`${newData.getFullYear()}`} </p>
-  //     </div>
-  //   </>
-  // 
-    contanierLogo,
-    createh,
-    contanierButton,
-    createClickP,
-    contanierYear
-
-
-)
-
+    <>
+     
+     
+     
+  )
 }
 
 export default App
